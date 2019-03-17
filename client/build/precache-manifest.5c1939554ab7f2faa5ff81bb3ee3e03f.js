a1c2742b54d8a0858c41fc5fa3a5bd3a8b3726ff
Karl <ke222pf@student.lnu.se>
2019-03-17 19:26:51 +0100

    push to production

---
+++ client/build/precache-manifest.5c1939554ab7f2faa5ff81bb3ee3e03f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4667e46f36b552de648c",
+    "url": "/static/css/main.18ed8350.chunk.css"
+  },
+  {
+    "revision": "4667e46f36b552de648c",
+    "url": "/static/js/main.4667e46f.chunk.js"
+  },
+  {
+    "revision": "df7b87c011876964da73",
+    "url": "/static/js/1.df7b87c0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "981104c49422af251995eb6330ef8a83",
+    "url": "/index.html"
+  }
+];
