8c9d38fbd4b106ee82e3056b544641cfb9f40727
Karl <ke222pf@student.lnu.se>
2019-03-17 22:38:51 +0100

    fixing production, and sockets on production

--- client/build/precache-manifest.4ffca89d516d737e15fa6d5cf64099a6.js
+++ client/build/precache-manifest.4ffca89d516d737e15fa6d5cf64099a6.js
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "1264ed9e23728e886924fb2297a58c92",
+    "revision": "defe7e1b8dc96e26e4f326df2cd447b5",
     "url": "/index.html"
   }
 ];
